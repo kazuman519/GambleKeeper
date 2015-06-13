eecde74e7100d8e88fce1b5e65139abfd84f3e2a
kazuman <noblesse-oblige_23@i.softbank.jp>
2015-06-13 21:33:04 +0900

    first commit

---
+++ GambleKeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GambleKeeper
+//
+//  Created by 三浦　和真 on 2015/06/13.
+//  Copyright (c) 2015年 三浦　和真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GambleKeeperTests/GambleKeeperTests.swift
@@ -0,0 +1,36 @@
+//
+//  GambleKeeperTests.swift
+//  GambleKeeperTests
+//
+//  Created by 三浦　和真 on 2015/06/13.
+//  Copyright (c) 2015年 三浦　和真. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GambleKeeperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
